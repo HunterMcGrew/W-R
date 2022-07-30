e41e2045383aa1f8315c1d4338cd02f341fdc726
Hunter McGrew <hunter109@gmail.com>
2022-07-30 17:45:59 -0500

    fixed messed up link for github pages in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
 
     return (
         <header className="d-flex vw-100 m-0 align-items-center justify-content-center">
-            <h1 className="headerText text-center m-0"><a href="/">Weather Dashboard</a></h1>
+            <h1 className="headerText text-center m-0"><a href="/W-R">Weather Dashboard</a></h1>
         </header>
     )
     
